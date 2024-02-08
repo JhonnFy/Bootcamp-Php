8e45eb40533e46b3095a828a6bb41d227c315ea5
JhonFy <dihonsdeveloper1023@gmail.com>
2024-02-08 12:31:56 -0500

    Variables

---
+++ A2_web2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    $nombre = "JhonFy";
+    $genero = "H";
+
+    print "Sea El Nombre De La Variable " . $nombre . " Genero: " . $genero . ".";
+    ?>
+
+</body>
+</html>
