cade6a311fbef96e0db2bd59336028f3ea0b8f00
JhonFy <dihonsdeveloper1023@gmail.com>
2024-02-08 09:16:32 -0500

    Hellow World

---
+++ A1_web1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+    // Este codigo se interpreta por el servidor
+        print 'Bienvenidos Al Curso De PHP <br>';
+        print 'Hola alumnos <br>';
+        print 'Este es el primer sitio web <br>';
+    ?>
+
+</body>
+</html>
