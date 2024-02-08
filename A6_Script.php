00f5b3204b2a55f1be5f80f04acb0eb4c3feee10
JhonFy <dihonsdeveloper1023@gmail.com>
2024-02-08 13:55:46 -0500

    Class

---
+++ A6_Script.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<style>
+.resaltar{
+    color:#40E0D0;
+    font-weight:bold;
+}
+</style>
+
+<?php
+print  "<p class='resaltar'>Esto es un ejemplo de parrafo</p>";
+?>
+
+
+</body>
+</html>
