9a44b7bf0fcf330bbe426e67dd669e1437976478
JhonFy <dijhonsdeveloper1023@gmail.com>
2024-02-12 23:43:43 -0500

    If Else

--- A14_Script.php
+++ A14_Script.php
@@ -61,7 +61,7 @@
             echo "<p class='frm'>Number In Range Cero To Ninety";
         }else if($vTwo >="91" && $vTwo <="100"){
             echo "<p class='frm'>Number In Range Cero To Hundred";
-        }else if($vTwo < "0" || $vTwo >"100"){
+        }else if($vTwo <= "0" || $vTwo >"100"){
             echo "<p class='frm'>Not Found";
         }
     }
