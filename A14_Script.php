c334f715db3c2ea7be5e09215d1fc7f04a3f5047
JhonFy <dijhonsdeveloper1023@gmail.com>
2024-02-12 23:29:05 -0500

    If

---
+++ A14_Script.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Numbers</title>
+
+<style>
+
+.txt{
+    color:#2E86C1;
+    text-align:center;
+}
+
+.frm{
+    color:#117A65;
+}
+
+.exit{
+    color:#D0D3D4;
+    background-color:#3498DB;
+}
+</style>
+
+</head>
+<body>
+
+<h1 class="txt">Personal Data</h1>
+
+<form action="" name="" id="" method="POST">
+    <!-- Name -->
+    <label class="frm" for="nm" name="lname" id="lname">Name</label><br>
+    <input type="text" name="dname" id="dname"><br>
+    <!-- Number -->
+    <label class="frm" for="lnumbers" name="" id="">Number</label><br>
+    <input type="text" name="dnumber" id="dnumber"><br><br>
+
+    <input type="submit" name="btn_go" id="btn_go" value="Goo">
+</form>
+
+<?php
+    if(isset($_POST["btn_go"])){
+        $vOne = $_POST["dname"];
+        $vTwo = $_POST["dnumber"];
+
+        if($vTwo <= "10"){
+            echo "<p class='frm'>Number In Range Cero To Ten.</p>";
+        }else if($vTwo >="11" && $vTwo <="20"){
+            echo "<p class='frm'>Number In Range Cero To Twenty.</p>";
+        }else if($vTwo >= "21" && $vTwo <="30"){
+            echo "<p class='frm'>Number In Range Ceto To Thirty";
+        }else if($vTwo >="31" && $vTwo <="40"){
+            echo "<p class='frm'>Number In Range Ceto To Forty";
+        }else if($vTwo >="41" && $vTwo <="50"){
+            echo "<p class='frm'>Number In Range Cero To Fifty";
+        }
+
+    }
+
+?>
+
+    
+</body>
+</html>
