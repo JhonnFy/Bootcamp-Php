0920a117f4593e27fd03c9ed51ffcfe71388ac51
JhonFy <dijhonsdeveloper1023@gmail.com>
2024-02-13 16:20:30 -0500

    Switch

---
+++ A15_Script.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Switch</title>
+
+<style>
+.ctg{
+    color:#2980B9;
+    background-color:#FBFCFC;
+}
+
+.ctgd{
+    color:#2980B9;
+    background-color:#ECF0F1;
+}
+
+.title{
+    color:#2980B9;
+    text-align:center;
+}
+
+.btn{
+    background-color:#2980B9;
+}
+</style>
+
+</head>
+<body>
+    
+<h1 class="title">Available Categories</h1>
+
+
+<table>
+    <tr>
+        <th>School</th>
+        <th class="btn"><input type="submit" value="Category One"></th>
+    </tr>
+    <tr>
+        <th>Toys</th>
+        <th class="btn"><input type="submit"></th>
+    </tr>
+    <tr>
+        <th>Animals</th>
+        <th class="btn"><input type="submit"></th>
+    </tr>
+    <tr>
+        <th>Clothes</th>
+        <th class="btn"><input type="submit"></th>
+    </tr>
+</table>
+
+
+
+
+<form action="">
+
+</form>
+
+</body>
+</html>
+
