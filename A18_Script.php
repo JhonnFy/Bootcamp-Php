86b19a728d6480556b74a59e0505bce5f673fa3e
JhonFy <dijhonsdeveloper1023@gmail.com>
2024-02-14 00:47:41 -0500

    Table Number 9

---
+++ A18_Script.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+    for($vUno = 0; $vUno<=10; $vUno++){
+        $num =9;
+        $op = ($num * $vUno);
+        echo  $num . "*" . $vUno . " = " . $op . ".<br>";
+       }
+?>
+</body>
+</html>
