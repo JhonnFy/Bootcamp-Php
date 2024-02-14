94c1bb935488e3aa6ba62adecea7fa14d2adf6dc
JhonFy <dijhonsdeveloper1023@gmail.com>
2024-02-14 00:05:29 -0500

    While

---
+++ A16_Script.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+    $var1 = 1;
+
+    while($var1 <6){
+        echo "Estamos Ejecutando El Script Del Bucle" . $var1 . "<br>";
+        $var1++;
+    }
+
+    echo "Fuera Del Bucle";
+?>
+
+</body>
+</html>
