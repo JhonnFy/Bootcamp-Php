3a4fa224558c358ddee4e66def6c50c90d5a5af5
JhonFy <dijhonsdeveloper1023@gmail.com>
2024-02-14 01:11:21 -0500

    Operator 9

--- A18_Script.php
+++ A18_Script.php
@@ -8,11 +8,17 @@
 <body>
     
 <?php
-    for($vUno = 0; $vUno<=10; $vUno++){
-        $num =9;
-        $op = ($num * $vUno);
-        echo  $num . "*" . $vUno . " = " . $op . ".<br>";
-       }
+
+       for($vUno = 0; $vUno<=10; $vUno++){
+            $num =9;
+            $mul = ($num * $vUno);
+            echo  $num . "*" . $vUno . " = " . $mul . ".<br>";
+        }
+
+       for($vDos = 10; $vDos>=0; $vDos--){
+            $nDos = ($vDos/9);
+            echo $vDos . " / 9 = " . $nDos . "<br>";
+        }
 ?>
 </body>
 </html>
